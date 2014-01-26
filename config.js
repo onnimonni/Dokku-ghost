95170517909bbe9a67adae4d095bdd8509702938
Onni Hakala <hakala.onni@gmail.com>
2014-01-26 20:50:23 +0200

    syntax errors with config.js

--- config.js
+++ config.js
@@ -49,11 +49,11 @@ config = {
         database: {
              client: 'postgres',
              connection: {
-                host: process.env.DB_HOST
-                user: process.env.DB_USER
-                password: process.env.DB_PASSWORD
-                database: process.env.DB_NAME
-                port: process.env.DB_PORT
+                host: process.env.DB_HOST,
+                user: process.env.DB_USER,
+                password: process.env.DB_PASSWORD,
+                database: process.env.DB_NAME,
+                port: process.env.DB_PORT,
             },
             debug: false
         },
